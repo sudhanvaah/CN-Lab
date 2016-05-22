ce897dd8db4f80f40c7c602ee4b34c1fcac71b6b
Sudhanva <sudhanvaah@users.noreply.github.com>
2016-05-22 16:48:05 +0530

    .

--- client.c
+++ client.c
@@ -1,20 +1,23 @@
 //client.c
-#include<stdlib.h>
 #include<fcntl.h>
-#include<netinet/in.h>
 #include<sys/socket.h>
-void main()
+#include<netinet/in.h>
+#include<stdlib.h>
+#include<stdio.h>
+int main(int argc,char **argv)
 {
-	char fname[255],buff[1024];
-	int n,cs;
-	struct sockaddr_in a;
-	a.sin_family=AF_INET;
-	a.sin_port=htons(15000);
-	inet_pton(AF_INET,argv[1],&a);
-	cs=socket(AF_INET,(sockaddr *)&a,sizeof(a));
-	printf("Enter filename ");
-	scanf("%s",fname);
-	send(cs,fname,255,0);
-	while(recv(cs,buff,1024,0))
-		printf("%s",buff);
+  int cs,n;
+  char buffer[1024],fname[255];
+  struct sockaddr_in a;
+  a.sin_family=AF_INET;
+  a.sin_port=htons(15000);
+  inet_pton(AF_INET,argv[1],&a.sin_addr);
+  cs=socket(AF_INET,SOCK_STREAM,0);
+  connect(cs,(struct sockaddr*)&a,sizeof(a));
+  printf("Enter filename ");
+  scanf("%s",fname);
+  send(cs,fname,255,0);
+  while(n=recv(cs,buffer,1024,0))
+  	printf("%s",buffer);
+  
 }
