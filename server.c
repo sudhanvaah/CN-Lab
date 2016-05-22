5e76c3e63116a29fa4f338422afd006dbec6b678
Sudhanva <sudhanvaah@users.noreply.github.com>
2016-05-22 13:58:34 +0530

    .

--- server.c
+++ server.c
@@ -1,64 +1,23 @@
 //server.c
-
-#include<stdio.h>
-
-#include<unistd.h>
-
-#include<fcntl.h>
-
-#include<sys/types.h>
-
-#include<sys/stat.h>
-
-#include<sys/socket.h>
-
-#include<netinet/in.h>
-
 #include<stdlib.h>
-
+#include<fcntl.h>
+#include<netinet/in.h>
+#include<sys/socket.h>
 int main()
-
 {
-
-  int cs,ns,fd,n;
-
-  int bufsize=1024;
-
-  char *buffer=malloc(bufsize);
-
-  struct sockaddr_in address;
-
-  char fname[255];
-
-  address.sin_family=AF_INET;
-
-  address.sin_port=htons(15000);
-
-  address.sin_addr.s_addr=
-INADDR_ANY;
-
-  cs=socket(AF_INET,SOCK_STREAM,0);
-
-  bind(cs,(struct sockaddr *)&address,sizeof(address));
-
-  listen(cs,3);
-
-  ns=accept(cs,(struct sockaddr *)NULL,NULL);
-
-  recv(ns,fname,255,0);
-
-  fd=open(fname,O_RDONLY);
-
-  n=read(fd,buffer,bufsize);
-
-  send(ns,buffer,n,0);
-
-  close(ns);
-
-  return close(cs);
-
+	int cs,ns,fd,n;
+	char buff[1024],fname[255];
+	struct sockaddr_in a;
+	a.sin_family=AF_INET;
+	a.sin_port=htons(15000);
+	a.sin_addr.s_addr=INADDR_ANY;
+	cs=socket(AF_INET,SOCK_STREAM,0);
+	bind(cs,(struct sockaddr *)&a,sizeof(a));
+	listen(cs,3);
+	ns=accept(cs,(struct sockaddr *)NULL,NULL);
+	recv(ns,fname,255,0);
+	fd=open(fname,O_RDONLY);
+	n=read(fd,buff,1024);
+	send(ns,buff,n,0);
+	return close(cs);
 }
-
-
-
-
